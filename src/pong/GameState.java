7981da9d84f5545b6db67c51d82160345f5833e6
Eren Taskiran <eren.tas3535@gmail.com>
2026-01-27 13:53:02 +0300

    Ortak Veri Yapıları ve UI İskeleti (Shared Data & UI)

--- src/pong/GameState.java
+++ src/pong/GameState.java
@@ -1,4 +1,21 @@
 package pong;
 
-public class GameState {
+import java.io.Serializable;
+
+public class GameState implements Serializable {
+    int ballX;
+    int ballY;
+    int paddleLeftY;
+    int paddleRightY;
+    int scoreLeft;
+    int scoreRight;
+
+    public GameState() {
+        ballX = 0;
+        ballY = 0;
+        paddleLeftY = 0;
+        paddleRightY = 0;
+        scoreLeft = 0;
+        scoreRight = 0;
+    }
 }
--- src/pong/Main.java
+++ src/pong/Main.java
@@ -1,15 +1,25 @@
 package pong;
 
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        for (long i = 0; i<10000; i++){
-            System.out.println(i);
-            try {
-                Thread.sleep(1000);
-            }catch (InterruptedException e){
-                e.printStackTrace();
-            }
-        }
+        GameState gameState = new GameState();
+        gameState.ballX = 400;
+        gameState.ballY = 250;
+        gameState.paddleLeftY = 200;
+        gameState.paddleRightY = 200;
+        gameState.scoreLeft = 3;
+        gameState.scoreRight = 5;
+
+        GamePanel gamePanel = new GamePanel();
+        gamePanel.gameState = gameState;
+
+        // Create JFrame window
+        JFrame frame = new JFrame("Distributed Pong");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+        frame.add(gamePanel);
+        frame.setVisible(true);
     }
 }
